54d2739ff96d9e4c7ffb29c81935e0aab964f368
Ali <alifayed.h@gmail.com>
2023-04-02 22:29:52 -0700

    More premake setup

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,13 @@
-include "dependencies.lua"
-
 workspace "Ulta"
-    architecture { "x86_64" }
-    configurations { "Debug", "Release" }
-    flags { "MultiProcessorCompile" }
+    architecture "x86_64"
+    
+    configurations 
+    { 
+        "Debug", "Release"
+    }
 
 output_dir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 group "core"
     include "ulta"
+group ""
--- ulta/premake5.lua
+++ ulta/premake5.lua
@@ -1,18 +1,19 @@
 project "ulta"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++20"
     staticruntime "off"
 
-    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. output_dir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. output_dir .. "/%{prj.name}")
 
-    pchheader "source/ultapch.h"
+    pchheader "source/ultapch.hpp"
 	pchsource "source/ultapch.cpp"
 
     files
 	{
-		"source/**.h",
-		"source/**.cpp",
+		"source/**.hpp",
+		"source/**.cpp"
 	}
 
     includedirs
@@ -20,20 +21,12 @@ project "ulta"
 		"source"
     }
 
-    links
-    {
-    }
-
     filter "configurations:Debug"
-    {
         defines "ULTA_DEBUG"
         runtime "Debug"
         symbols "on"
-    }
 
     filter "configurations:Release"
-    {
         defines "ULTA_RELEASE"
         runtime "Release"
         optimize "on"
-    }
